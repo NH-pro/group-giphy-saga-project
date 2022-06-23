f0899fa00b8082b95352ee636d72f3777b9ca5e7
Colin Oraskovich <Colin.Oraskovich@gmail.com>
2022-06-23 14:28:49 -0500

    Modified the SQL file to use FK references and populate it with some dummy-data

--- database.sql
+++ database.sql
@@ -17,6 +17,13 @@ VALUES ('funny'), ('cohort'), ('cartoon'), ('nsfw'), ('meme');
 CREATE TABLE "favorites" (
     "id" SERIAL PRIMARY KEY,
     "gif_id" VARCHAR NOT NULL,
-    "category_id" VARCHAR NOT NULL,
+    "category_id" INT REFERENCES "category" NOT NULL,
     "url" VARCHAR NOT NULL
-)
+);
+
+-- Create some dummy-data to start with
+INSERT INTO "favorites"
+	("gif_id", "category_id", "url")
+VALUES
+	('xT4uQulxzV39haRFjG', 1, 'https://media0.giphy.com/media/xT4uQulxzV39haRFjG/100w.gif?cid=0f174abbb48b0f945d73b1ab62621b5ff3cbb06432c26ec0&rid=100w.gif&ct=g'),
+	('dLvzDjRKoCxRLaTV3p', 2, 'https://media1.giphy.com/media/dLvzDjRKoCxRLaTV3p/giphy.gif?cid=0f174abbb98ff5de40eb9de82209c3b0e20463209a344ca7&rid=giphy.gif&ct=g');
