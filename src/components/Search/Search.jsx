454f569d1bcedb25ba3c25dd3830cc37b1ed28fd
Neil Hanson <pro.neilhanson@gmail.com>
2022-06-23 16:11:59 -0500

    updated Search comp

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,20 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 
-
 function Search() {
-    const dispatch = useDispatch();
-    const [category, setCategory] = useState('')
+    // gif data store
+    const gif = useSelector(store => store.gif);
+    // category name store
+    const category = useSelector(store => store.category);
 
+    const dispatch = useDispatch();
+    const [userCategory, setUserCategory] = useState('')
+
+    // On page load, dispatch to index saga to fetch cat names.
+    useEffect(() => {
+        console.log(`Grabbing Category names from DB`);
+        dispatch({
+            type: 'GET_CATEGORY_NAMES'
+        })
+    }, [])
+
+    // Dropdown selector change handler
     const handleCatChange = (event) => {
-        setCategory(event.target.value);
+        setUserCategory(event.target.value);
     }
 
-    const searchGif = () => {
+    // When form submits, dispatch to search for a new gif.
+    const searchGif = (event) => {
+        event.preventDefault();
         dispatch({
             type: 'SEARCH_GIF',
-            payload: category
+            payload: userCategory
         })
     }
 
@@ -22,14 +37,14 @@ function Search() {
     return (
         <>
             <form onSubmit={searchGif}>
-                <select onChange={handleCatChange} id="cat_dropdown" name="cat_dropdown">
+                <select onChange={handleCatChange}>
+                    {/* map through category */}
                     {category.map((cat) => (
-                        <option value={cat.name}>{cat.name}</option>
+                        <option key={cat.name} value={cat.name}>{cat.name}</option>
                     ))}
                 </select>
                 <button type='submit'>Search for Gif</button>
             </form>
-
         </>
     )
 };
