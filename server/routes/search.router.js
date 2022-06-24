dd8f69a7d70f3d3fe2ddf4855c1bcd01dae3c067
Neil Hanson <pro.neilhanson@gmail.com>
2022-06-24 14:01:53 -0500

    got user search input to work

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -3,9 +3,9 @@ const axios = require('axios');
 const express = require('express');
 const router = express.Router();
 
-router.get('/:category', (req, res) => {
+router.get('/:category/:searchTerm', (req, res) => {
 
-    console.log(`In Router GET /search`, req.params.category);
+    console.log(`In Router GET /search`, req.params.category, req.params.searchTerm);
     console.log(process.env.GIPHY_API_KEY, "???")
 
     axios({
@@ -13,11 +13,11 @@ router.get('/:category', (req, res) => {
         url: 'https://api.giphy.com/v1/gifs/search',
         params: {
             api_key: process.env.GIPHY_API_KEY,
-            q: req.params.category
+            q: `${req.params.category} ${req.params.searchTerm}`
         }
     })
     .then(apiRes => {
-        console.log('Router GET /search request success!', apiRes.data);
+        //console.log('Router GET /search request success!', apiRes.data);
         res.send(apiRes.data);
     })
     .catch(err => {
